91a0988148020c5a86b27f18fcb55a7ceaf8ba26
unknown <strenadkosa@mail.ru>
2022-08-10 13:13:25 +0300

    выполнили 3 задание

---
+++ task003/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Введите целое число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+case 1:
+    {
+        Console.WriteLine("Понедельник");
+    break;
+    }
+
+case 2:
+    {
+        Console.WriteLine("Вт");
+    break;
+    }
+
+
+case 3:
+    {
+        Console.WriteLine("Ср");
+    break;
+    }
+
+
+case 4:
+    {
+        Console.WriteLine("Чт");
+    break;
+    }
+
+
+case 5:
+    {
+        Console.WriteLine("Пт");
+    break;
+    }
+
+
+case 6:
+    {
+        Console.WriteLine("Сб");
+    break;
+    }
+
+
+case 7:
+    {
+        Console.WriteLine("Воскр");
+    break;
+    }
+
+
+
+
+}
