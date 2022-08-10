b8171322d6f7e13fc2e956a96379d89eefa7a4a1
unknown <strenadkosa@mail.ru>
2022-08-10 12:59:11 +0300

    выполнили 2 задание

---
+++ task002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n == n*m)
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+}
