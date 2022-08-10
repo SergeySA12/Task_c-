e092e6d13753653c4d49de716dd41d61cdbe3a36
unknown <strenadkosa@mail.ru>
2022-08-10 14:13:36 +0300

    программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел

---
+++ task006/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int max = n;
+
+if (n > max) max = n;
+if (m > max) max = m;
+if (z > max) max = z;
+
+Console.Write("max = ");
+Console.WriteLine(max);
