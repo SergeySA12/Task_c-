a7c051e7965246a418264aeae8cddc2825b7fb85
unknown <strenadkosa@mail.ru>
2022-08-10 13:52:14 +0300

    программу, которая на вход принимает два числа и выдаёт максимальное

---
+++ task005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n > m)
+{
+    Console.WriteLine("первое целое число = max");
+}
+
+else
+{
+    Console.WriteLine("второе целое число = max");
+}
