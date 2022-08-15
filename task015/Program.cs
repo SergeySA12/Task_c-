2f4973d17b66ee1b6fb1b7a93b1d23d9cbebf18c
unknown <strenadkosa@mail.ru>
2022-08-15 18:52:58 +0300

    Задача 2 Домашние задание семинара 2

---
+++ task015/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи число: n ");
+int n = Convert.ToInt32(Console.ReadLine()); // n = любые числа
+string nText = Convert.ToString(n);
+if (nText.Length > 2)
+{
+  Console.WriteLine("Третья цифра данного числа" + nText[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет");
+}
