e33269cf218297996472acd3b1d55469476bf38a
unknown <strenadkosa@mail.ru>
2022-08-15 11:38:59 +0300

    Задача с масивами

---
+++ task009/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = new Random().Next(10, 100);
+
+Console.WriteLine("Случайное число от 10 до 99: " + n);
+
+int FirstDigit = n / 10;
+int SecondDigit = n % 10;
+
+if (FirstDigit > SecondDigit)
+ {
+    Console.WriteLine("Наибольшая цифра: " + FirstDigit);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра: " + SecondDigit);
+}
