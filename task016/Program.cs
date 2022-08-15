bfd51e35c86b5ef0d14ad6a5b5b36fac63c4e76e
unknown <strenadkosa@mail.ru>
2022-08-15 19:09:32 +0300

    Задача 3 Домашние задание семинара 2

---
+++ task016/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи  цифру дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7)
+   {
+  Console.WriteLine("Это выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7)
+   {
+    Console.WriteLine("это вообще не день недели");
+  }
+  else Console.WriteLine("Это не выходной день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
