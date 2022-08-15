bcd7e97a8f3375700ef90a331d132e63afe55f37
unknown <strenadkosa@mail.ru>
2022-08-15 18:24:48 +0300

    Задача 5 Семинар 2

---
+++ task013/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if ((n / m ==m)^(m / n ==n))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Не является");
+
+}
