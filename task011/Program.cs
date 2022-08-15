1ce7f9e87dd3468019df0d96168fe229f2055d60
unknown <strenadkosa@mail.ru>
2022-08-15 12:17:07 +0300

    Задача 3 Семинар 2

---
+++ task011/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+
+if(n % m == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Остаток" + n % m);
+}
