70f3f08df0eda10bf9058df9b363498f45b76742
unknown <strenadkosa@mail.ru>
2022-08-17 16:17:56 +0300

    Задача 4 Домашние задание семинара 3

---
+++ Task019/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов от 1 до N
+// 5
+// 2
+
+
+Console.WriteLine("Введи число N: ");
+int N = int.Parse(Console.ReadLine());
+int index = 1;
+ while ( index <= Math.Abs(N))
+ {
+Console.Write($"{index * index}, ");
+index ++;
+ }
+ 
+ 
