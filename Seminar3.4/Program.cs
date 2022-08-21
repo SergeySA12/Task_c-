19094e6f5c13b15c601404dddf72eaee7d11b0e6
unknown <strenadkosa@mail.ru>
2022-08-21 12:48:45 +0300

    Семинар 3 Задание 4

---
+++ Seminar3.4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу. которая массив из 8 элементов. заполненый нулями и единицами в случайном порядке.
+
+int[] mass = new int[8];
+int i = 0;
+while ( i < mass.Length)
+{
+    mass[i] = new Random().Next(0,2);
+    Console.Write(mass[i]);
+    i++;
+}
