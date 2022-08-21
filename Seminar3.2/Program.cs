3b5f3a9dae72921ef08f3ec2cd998866a8d452b3
unknown <strenadkosa@mail.ru>
2022-08-21 11:59:30 +0300

    Семинар 3 Задание 2

---
+++ Seminar3.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу. которая принимает на вход число (А) и выдает количество цифр в числе.
+
+Console.Write("Введи число: n ");
+int n =  Math.Abs(Convert.ToInt32(Console.ReadLine())); // n = любые числа
+int count = 0;
+while (n != 0)
+{
+    count++;
+    n = n / 10;
+}
+Console.WriteLine($" в числе {count} цифр");
