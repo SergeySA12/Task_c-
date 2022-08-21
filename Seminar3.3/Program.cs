552202c55ef8abad5fc48ace914dc59c9c559970
unknown <strenadkosa@mail.ru>
2022-08-21 12:17:54 +0300

    Семинар 3 Задание 3

---
+++ Seminar3.3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу. которая принимает на вход число (А) и выдает произведение чисел от 1 до А
+
+Console.WriteLine("Введи число: n ");
+int n =  Convert.ToInt32(Console.ReadLine());
+int count = 1;
+for (int i = 2; i <= n; i++)
+{
+    count = count * i;
+}
+Console.WriteLine(count);
