e93e4dcbaf656fd8b0ce0f8a4a2e7ee39dd28c33
unknown <strenadkosa@mail.ru>
2022-08-21 11:28:00 +0300

    Семинар 3 Задание 1

---
+++ Seminar3.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу. которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+
+Console.WriteLine("Введи  число (n): ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"{sum}");
