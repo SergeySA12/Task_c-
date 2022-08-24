cc7a30f541ed9824631cdd18f53bd496a48f57ab
unknown <strenadkosa@mail.ru>
2022-08-24 13:14:26 +0300

    Задание №3 Семинар 5

---
+++ Seminar5.3/Program.cs
@@ -0,0 +1,40 @@
+﻿// задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+void FillArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr [i] = new Random().Next (0,150);
+    }
+}
+
+void PrintArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr [i]} ");
+    }
+}
+int [] array = new int [123];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 10 & array[i] < 99);
+    {
+        // Console.WriteLine($"Заданнное число присутсвует в массиве");
+        count ++;
+    }
+}
+Console.WriteLine(count);
