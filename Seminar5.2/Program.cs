92c81051936ecb47a2e7c47d8cec810a3cc8a4d6
unknown <strenadkosa@mail.ru>
2022-08-24 12:51:13 +0300

    Задание №2 Семинар 5

---
+++ Seminar5.2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+void FillArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        arr [i] = new Random().Next (-100,100);
+    }
+}
+
+void PrintArray (int [] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{arr [i]} ");
+    }
+}
+int [] array = new int [8];
+
+FillArray(array);
+PrintArray(array);
+
+
+Console.WriteLine("Введи  число : ");
+int a = int.Parse(Console.ReadLine());
+
+
+int var = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (a == array[i]);
+    {
+        Console.WriteLine($"Заданнное число присутсвует в массиве");
+        var ++;
+        break;
+    }
+}
+
+if (var == 0)
+{
+    Console.WriteLine($"Заданнное число отуствует");
+} 
